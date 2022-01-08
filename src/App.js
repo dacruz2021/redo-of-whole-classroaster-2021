c26a0634070a2866cfb8ba2f9f6e59f10cef3333
dacruz2021 <72949859+dacruz2021@users.noreply.github.com>
2022-01-08 10:39:52 -0600

    2D array called and rendered with comments I finally got my code to render and figured out how for loops work with 2D arrays :)

--- src/App.js
+++ src/App.js
@@ -1,43 +1,42 @@
 import "./styles.css";
-//previous function that called people
-//function people(first, last, pronouns, year, color, animal) {
-//return (
-//<div class="row">
-//<div class="blue">{first}</div>
-//<div>{last}</div>
-//<div class="blue">{pronouns}</div>
-//<div>{year}</div>
-//<div>{color}</div>
-//<div class="blue">{animal}</div>
-//</div>
-//);
-//}
 
-function people(first, last) {
+function people(first, last, pronouns, year, color, animal) {
   //this is not necessary but why does the 2D array not render?
-  <div class="row">
-    <div> {first}</div>
-    <div>{last}</div>
-  </div>;
+  // you need to return the information in jsx ( use {}) and then call the parameters so it will render
+  return (
+    <p>
+      {" "}
+      {first} {last} {pronouns} {year} {color} {animal}{" "}
+    </p>
+  );
 }
 export default function App() {
-  //these are from prvious session and can be called in the App div with {denny}
-  //const denny = people("Denny", "Fernandexz", "He/His", 2022, "Blue", "Dogs");
-  //const heber = people("Heber","Cruz-Hernandez","He/His","2023","Green","Cats");
-
-  // "you" array that contains data for first and last in the people function
+  //this is a 2d array that holds the information from each individual and is later called in the for loop
   let you = [
-    ["Denny", "Fernandez"],
-    ["Heber", "Cruz-Hernandez"]
+    ["Denny", "Fernandez", "He/His", "2022", "Blue", "Dogs"],
+    ["Heber", "Cruz-Hernandez", "He/His", "2023", "Green", "Cats"],
+    ["Mateo", "Cruz", "He/His", "2024", "Yellow", "Dogs"]
   ];
-  // blank array called elements that is supposed to hold the information from "you" once ran through the for
-  //loop
+  // let elements reamain blank so that the for loop will run and input it later
   let elements = [];
   //for loop that tries to gather information from "you" and push the information to elements to be displayed
+  // hold each parameter as a variable to that you can assaign it a piece of information from the
+  // you 2d array. As it itterates. each variable is assigned the parameter from the you 2d array
+  // we use .push to push the information inoto the elements array that is later called in our
+  // return App.
   for (let u of you) {
+    // first is the parameter of people and is then assaigned the [0] piece of inforamtion in the 2d array
+    // for people. This continues until every parameter in the people function is called
     let first = u[0];
     let last = u[1];
-    elements.push(people(first, last));
+    let pronouns = u[2];
+    let year = u[3];
+    let color = u[4];
+    let animal = u[5];
+    //elements.push is where the information is going once the for loop has ran through
+    //we call people with all of the parameters since the information is now updated and the
+    // elements array now has information to call on.
+    elements.push(people(first, last, pronouns, year, color, animal));
   }
 
   return (
