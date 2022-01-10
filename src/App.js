e02802b666e595da84b9fc8163dd990180b68d91
dacruz2021 <72949859+dacruz2021@users.noreply.github.com>
2022-01-10 15:35:43 -0600

    Failed attempt at using .map I looked through the slides in the review sections but found failed to implement what was taught.

--- src/App.js
+++ src/App.js
@@ -1,48 +1,19 @@
 import "./styles.css";
 
-function people(first, last, pronouns, year, color, animal) {
-  //this is not necessary but why does the 2D array not render?
-  // you need to return the information in jsx ( use {}) and then call the parameters so it will render
-  return (
-    <p>
-      {" "}
-      {first} {last} {pronouns} {year} {color} {animal}{" "}
-    </p>
-  );
-}
+function people() {}
+let you = [
+  ["Denny", "Fernandez", "He/His", "2022", "Blue", "Dogs"],
+  ["Heber", "Cruz-Hernandez", "He/His", "2023", "Green", "Cats"],
+  ["Mateo", "Cruz", "He/His", "2024", "Yellow", "Dogs"]
+];
+//I tried to use the .map notation but I am still having some trouble using it
 export default function App() {
-  //this is a 2d array that holds the information from each individual and is later called in the for loop
-  let you = [
-    ["Denny", "Fernandez", "He/His", "2022", "Blue", "Dogs"],
-    ["Heber", "Cruz-Hernandez", "He/His", "2023", "Green", "Cats"],
-    ["Mateo", "Cruz", "He/His", "2024", "Yellow", "Dogs"]
-  ];
-  // let elements reamain blank so that the for loop will run and input it later
-  let elements = [];
-  //for loop that tries to gather information from "you" and push the information to elements to be displayed
-  // hold each parameter as a variable to that you can assaign it a piece of information from the
-  // you 2d array. As it itterates. each variable is assigned the parameter from the you 2d array
-  // we use .push to push the information inoto the elements array that is later called in our
-  // return App.
-  for (let u of you) {
-    // first is the parameter of people and is then assaigned the [0] piece of inforamtion in the 2d array
-    // for people. This continues until every parameter in the people function is called
-    let first = u[0];
-    let last = u[1];
-    let pronouns = u[2];
-    let year = u[3];
-    let color = u[4];
-    let animal = u[5];
-    //elements.push is where the information is going once the for loop has ran through
-    //we call people with all of the parameters since the information is now updated and the
-    // elements array now has information to call on.
-    elements.push(people(first, last, pronouns, year, color, animal));
-  }
+  let personInfo = you.map(people);
 
   return (
     <div className="App">
       <h1>First Step to Student roaster Project</h1>
-      {elements}
+      {personInfo}
     </div>
   );
 }
